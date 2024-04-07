399baf7b47825f506e694c5352bffd32532bc6c4
apollo <Apollo-74@ya.ru>
2024-04-07 09:34:11 +0500

    Edit settings virtualbox

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -3,5 +3,4 @@ _: {
   users.extraGroups.vboxusers.members = [ "apollo" ];
   virtualisation.virtualbox.host.enableExtensionPack = true;
   virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;
 }
