7e235f1b2cf04e9052d5d06fdc864d02a07dcbe2
apollo <Apollo-74@ya.ru>
2024-04-07 09:05:59 +0500

    Removed duplicate 'unfree' options and edited file virtualbox

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -1,4 +1,7 @@
 _: {
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "apollo" ];
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.x11 = true;
 }
