2b3c422cd4688f8e7f8f81ee33f9d949e6598f1a
apollo <Apollo-74@ya.ru>
2024-08-17 13:45:03 +0500

    add supported locales "ru"

--- modules/locale.nix
+++ modules/locale.nix
@@ -1,5 +1,9 @@
 {
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "ru_RU.UTF-8/UTF-8"
+  ];
   time.hardwareClockInLocalTime = true;
   time.timeZone = "Asia/Yekaterinburg";
 }
