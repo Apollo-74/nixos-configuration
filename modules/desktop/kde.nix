a358415d6ab098f83e7e18ebc1701fef3e1cb936
apollo <Apollo-74@ya.ru>
2024-09-22 11:52:19 +0500

    Exclude kde package "discover"

--- modules/desktop/kde.nix
+++ modules/desktop/kde.nix
@@ -3,7 +3,10 @@
   services = {
     desktopManager.plasma6.enable = true;
   };
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [ konsole ];
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    discover
+  ];
   hardware.graphics.enable = true;
   networking.networkmanager.enable = true;
 }
