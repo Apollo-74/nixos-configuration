a7902090a9ac8414aefd0a0a17fffac084d3a63f
apollo <Apollo-74@ya.ru>
2024-11-23 21:22:56 +0500

    add virtualbox

--- modules/desktop/programs.nix
+++ modules/desktop/programs.nix
@@ -80,4 +80,8 @@
 
   virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
 }
