beeabcab9e708d0837cfddfa5cebe0d90c17723b
apollo <Apollo-74@ya.ru>
2025-01-18 21:52:33 +0500

    remove name-snrl ssh-keys

--- modules/servers/openssh.nix
+++ modules/servers/openssh.nix
@@ -4,10 +4,5 @@
     settings.PermitRootLogin = "no";
     settings.PasswordAuthentication = false;
   };
-  users.users.default.openssh.authorizedKeys.keyFiles = [
-    #(__fetchurl {
-    #  url = "https://github.com/name-snrl.keys";
-    #  sha256 = "";
-    #})
-  ];
+  users.users.default.openssh.authorizedKeys.keyFiles = [ ];
 }
